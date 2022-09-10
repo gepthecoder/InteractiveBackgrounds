f273243a1ddfbe21b0dee5f1b875e6a8041d36be
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-09-10 12:41:10 +0200

    $0021: [Blobio]: change back to unity default render pipeline

--- InteractiveBackgrounds/InteractiveBackgrounds/Assets/3dBackgrounds/Scripts/TransparentWindow.cs
+++ InteractiveBackgrounds/InteractiveBackgrounds/Assets/3dBackgrounds/Scripts/TransparentWindow.cs
@@ -56,7 +56,7 @@ public class TransparentWindow : MonoBehaviour
         DwmExtendFrameIntoClientArea(hWnd, ref margins);
 
         SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
-        //SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
+        SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
 
         SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
 #endif
